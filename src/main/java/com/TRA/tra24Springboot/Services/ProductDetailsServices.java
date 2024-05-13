8f7f269b1a22945ce384fba1d67cff08a4f1ad13
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-13 11:45:27 +0400

    created ProductDetailsRepository

---
+++ src/main/java/com/TRA/tra24Springboot/Services/ProductDetailsServices.java
@@ -0,0 +1,10 @@
+package com.TRA.tra24Springboot.Services;
+
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductDetailsServices {
+
+}
