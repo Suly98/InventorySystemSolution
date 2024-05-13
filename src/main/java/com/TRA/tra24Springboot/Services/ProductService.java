02dba2b293214c7ceea830ac77c4b52323133e57
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-13 09:36:37 +0400

    created ProductService

---
+++ src/main/java/com/TRA/tra24Springboot/Services/ProductService.java
@@ -0,0 +1,23 @@
+package com.TRA.tra24Springboot.Services;
+
+import com.TRA.tra24Springboot.InvoiceRepositories.ProductRepository;
+import com.TRA.tra24Springboot.Models.Product;
+import com.TRA.tra24Springboot.Models.ProductDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+
+
+}
