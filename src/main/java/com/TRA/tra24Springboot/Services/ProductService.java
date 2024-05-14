57b9ea1d46301fc0c7260372e87d7b66d465f1a8
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-14 20:18:51 +0400

    added DTO getAll function

--- src/main/java/com/TRA/tra24Springboot/Services/ProductService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/ProductService.java
@@ -1,5 +1,6 @@
 package com.TRA.tra24Springboot.Services;
 
+import com.TRA.tra24Springboot.DTO.ProductDTO;
 import com.TRA.tra24Springboot.InvoiceRepositories.ProductDetailsRepository;
 import com.TRA.tra24Springboot.InvoiceRepositories.ProductRepository;
 import com.TRA.tra24Springboot.Models.Product;
@@ -8,9 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Arrays;
-import java.util.Date;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class ProductService {
@@ -23,13 +22,23 @@ public class ProductService {
     ProductDetailsServices productDetailsServices;
 
 
-    public Product addProduct(Product product){
+    public ProductDTO addProduct(Product product){
         product.setSku(UUID.randomUUID());
         product.setIsActive(true);
         product.setCreatedDate(new Date());
 
         productDetailsServices.addToProduct(product.getProductDetails());
 
-        return productRepository.save(product);
+        productRepository.save(product);
+
+        ProductDTO dto = ProductDTO.convertToDTO(product);
+
+        return dto;
+
+    }
+
+    public List<ProductDTO> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        return ProductDTO.convertToDTO(products);
     }
 }
