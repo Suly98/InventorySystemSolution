06a3ebabb717cbd2ee13b6b7bb48314f0edf1c5e
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-15 12:18:54 +0400

    created query to get by id

--- src/main/java/com/TRA/tra24Springboot/InvoiceRepositories/ProductRepository.java
+++ src/main/java/com/TRA/tra24Springboot/InvoiceRepositories/ProductRepository.java
@@ -2,7 +2,11 @@ package com.TRA.tra24Springboot.InvoiceRepositories;
 
 import com.TRA.tra24Springboot.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    @Query("SELECT i FROM Product i WHERE i.id =:id")
+    Product getProductById(@Param("id") Integer id);
 
 }
