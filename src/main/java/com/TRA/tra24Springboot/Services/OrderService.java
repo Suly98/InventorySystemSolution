f4aa920837da3fbafcb7bebdd095f72913040a8c
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-05-27 12:10:55 +0400

    added deleteOrderbyId()

--- src/main/java/com/TRA/tra24Springboot/Services/OrderService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/OrderService.java
@@ -3,6 +3,7 @@ package com.TRA.tra24Springboot.Services;
 import com.TRA.tra24Springboot.DTO.OrderDTO;
 import com.TRA.tra24Springboot.InvoiceRepositories.OrderRepository;
 import com.TRA.tra24Springboot.Models.Order;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,5 +24,15 @@ public class OrderService {
 
         OrderDTO dto = OrderDTO.convertToDTO(order);
         return dto;
+    }
+
+    public String deleteOrderById(Integer id){
+        for(Order order: orderRepository.findAll()){
+            if(orderRepository.existsById(id)){
+                orderRepository.deleteById(id);
+                return "Order id " + id + " deleted successfuly";
+            }
+        }
+        return "Did not delete";
     }
 }
