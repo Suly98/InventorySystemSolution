0ca1b552fe2dee058e7f666312862e88d1d13916
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-02 22:58:48 +0400

    added SlackController

---
+++ src/main/java/com/TRA/tra24Springboot/Controllers/SlackController.java
@@ -0,0 +1,21 @@
+package com.TRA.tra24Springboot.Controllers;
+
+import com.TRA.tra24Springboot.Services.SlackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("Slack")
+public class SlackController {
+
+    @Autowired
+    SlackService slackService;
+
+    @PostMapping("m")
+    public void sendMessage(){
+        slackService.sendMessage("random","hello");
+    }
+}
