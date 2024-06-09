4d32a8e428e161b0f6b618e0b6176e35ec2cb55f
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-06-09 10:16:38 +0400

    created sendSimpleMail()

--- src/main/java/com/TRA/tra24Springboot/Services/MailingService.java
+++ src/main/java/com/TRA/tra24Springboot/Services/MailingService.java
@@ -1,5 +1,29 @@
 package com.TRA.tra24Springboot.Services;
 
-public class MailingService {
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
+
+@Service
+public class MailingService {
+    private JavaMailSender javaMailSender;
+
+    private String sender;
+
+    public String sendSimpleMail(String receiver, String sender,
+                                 String emailBody, String subject ){
+        try{
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(sender);
+            mailMessage.setTo(receiver);
+            mailMessage.setText(emailBody);
+            mailMessage.setSubject(subject);
+            mailMessage.setSentDate(new Date());
+            return "success!";
+        } catch (Exception e){
+            return "Error!";
+        }
+    }
 }
