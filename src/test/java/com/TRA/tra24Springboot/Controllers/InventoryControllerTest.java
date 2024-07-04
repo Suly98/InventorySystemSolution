047665c919cf0eb681767497fc31fade90ee4219
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-04 10:46:54 +0400

    generated InventoryControllerTest.java

---
+++ src/test/java/com/TRA/tra24Springboot/Controllers/InventoryControllerTest.java
@@ -0,0 +1,24 @@
+package com.TRA.tra24Springboot.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryControllerTest {
+
+    @Test
+    void addStock() {
+    }
+
+    @Test
+    void deleteInventory() {
+    }
+
+    @Test
+    void getInventories() {
+    }
+
+    @Test
+    void getByManager() {
+    }
+}
