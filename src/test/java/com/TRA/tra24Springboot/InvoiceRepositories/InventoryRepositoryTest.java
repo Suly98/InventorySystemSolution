774b71c1f9e667a2f40e764a68345ff96244db42
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-08 12:26:56 +0400

    created getByLocation function

--- src/test/java/com/TRA/tra24Springboot/InvoiceRepositories/InventoryRepositoryTest.java
+++ src/test/java/com/TRA/tra24Springboot/InvoiceRepositories/InventoryRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.TRA.tra24Springboot.InvoiceRepositories;
 
 import com.TRA.tra24Springboot.Models.Inventory;
+import org.assertj.core.api.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,19 +26,24 @@ class InventoryRepositoryTest {
     void setUp(){
         Inventory inventory1 = Inventory.builder().location("Russel City")
                 .manager("Mohammed")
+                .supplier("Ahmed")
                 .build();
 
         Inventory inventory2 = Inventory.builder().location("Sinaw City")
                 .manager("Nasser")
                 .supplier("abu hamood")
                 .build();
+
+        inventoryRepository.save(inventory2);
+        inventoryRepository.save(inventory1);
     }
 
     @Test
-    void getByLocationName() {
+    public void getByLocationName() {
+        List<Inventory> inventory = inventoryRepository.getByLocationName("Sinaw City");
+        Assertions.assertThat(inventory).isNotNull();
+        Assertions.assertThat(inventory.getFirst().getLocation()).isEqualTo("Sinaw City");
     }
 
-    @Test
-    void getByName() {
-    }
+
 }
