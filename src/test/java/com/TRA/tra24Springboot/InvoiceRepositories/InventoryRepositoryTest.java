b97458413d87990797b6a898aa09a0a3c46178ad
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-08 11:59:51 +0400

    added before each setUp function

--- src/test/java/com/TRA/tra24Springboot/InvoiceRepositories/InventoryRepositoryTest.java
+++ src/test/java/com/TRA/tra24Springboot/InvoiceRepositories/InventoryRepositoryTest.java
@@ -1,7 +1,10 @@
 package com.TRA.tra24Springboot.InvoiceRepositories;
 
+import com.TRA.tra24Springboot.Models.Inventory;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,14 +12,24 @@ import org.springframework.test.context.ActiveProfiles;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-
 @ActiveProfiles("test")
-
-
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class InventoryRepositoryTest {
 
     @Autowired
+    private InventoryRepository inventoryRepository;
 
+    @BeforeEach
+    void setUp(){
+        Inventory inventory1 = Inventory.builder().location("Russel City")
+                .manager("Mohammed")
+                .build();
+
+        Inventory inventory2 = Inventory.builder().location("Sinaw City")
+                .manager("Nasser")
+                .supplier("abu hamood")
+                .build();
+    }
 
     @Test
     void getByLocationName() {
