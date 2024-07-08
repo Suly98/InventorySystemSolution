58316377bb4e874113d8246d81d4321760e87c3e
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-08 13:42:21 +0400

    created addStock for testing

--- src/test/java/com/TRA/tra24Springboot/Controllers/InventoryControllerTest.java
+++ src/test/java/com/TRA/tra24Springboot/Controllers/InventoryControllerTest.java
@@ -1,16 +1,20 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.DTO.InventoryDTO;
 import com.TRA.tra24Springboot.InvoiceRepositories.InventoryRepository;
+import com.TRA.tra24Springboot.Models.Inventory;
 import com.TRA.tra24Springboot.Services.InventoryService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.*;
+import org.mockito.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class InventoryControllerTest {
+
 
     @Mock
     private InventoryRepository inventoryRepository;
@@ -28,17 +32,36 @@ class InventoryControllerTest {
 
     @Test
     void addStock() {
+        String location = "Sur";
+        String manager = "Sulaiman";
+        // prepare Mock Date:
+        Inventory inventory = Inventory.builder()
+                .manager(manager)
+                .location(location)
+                .phoneNumber("9779")
+                .build();
+
+        inventory.setIsActive(true);
+        inventory.setLocation("Sur");
+
+        when(inventoryRepository.save(any(Inventory.class))).thenReturn(inventory);
+
+        InventoryDTO savedInventory = inventoryService.addInventory(inventory);
+
+        verify(inventoryRepository, times(1)).save(any(Inventory.class));
+
+        assertEquals("Sulaiman", savedInventory.getInventoryManager());
     }
 
-    @Test
-    void deleteInventory() {
-    }
-
-    @Test
-    void getInventories() {
-    }
-
-    @Test
-    void getByManager() {
-    }
+//    @Test
+//    void deleteInventory() {
+//    }
+//
+//    @Test
+//    void getInventories() {
+//    }
+//
+//    @Test
+//    void getByManager() {
+//    }
 }
