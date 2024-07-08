05c570bb26589bd8490b43100b59a001a45891b7
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-08 11:35:36 +0400

    added mock injections

--- src/test/java/com/TRA/tra24Springboot/Controllers/InventoryControllerTest.java
+++ src/test/java/com/TRA/tra24Springboot/Controllers/InventoryControllerTest.java
@@ -1,10 +1,22 @@
 package com.TRA.tra24Springboot.Controllers;
 
+import com.TRA.tra24Springboot.InvoiceRepositories.InventoryRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryControllerTest {
+
+    @Mock
+    private InventoryRepository inventoryRepository;
+
+
+
+    @InjectMocks
+    private InventoryController inventoryController;
+
 
     @Test
     void addStock() {
