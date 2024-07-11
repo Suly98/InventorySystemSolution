964cdb14392ad2db893e3d5d13656e565e094369
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-11 10:14:25 +0400

    added comments

--- src/test/java/com/TRA/tra24Springboot/Services/InventoryServiceTest.java
+++ src/test/java/com/TRA/tra24Springboot/Services/InventoryServiceTest.java
@@ -16,10 +16,20 @@ import static org.mockito.Mockito.*;
 
 class InventoryServiceTest {
 
-
+    /*
+    This line tells Mockito to create a mock instance of the SchoolRepository class.
+    This mock can then be used to simulate interactions with the repository
+    in a controlled way during your tests.
+    */
     @Mock
     private InventoryRepository inventoryRepository;
 
+
+    /*
+    This tells Mockito to create an instance of the SchoolService class
+    and automatically inject the mock instance of SchoolRepository into it.
+    This allows you to test SchoolService in isolation by using the mock SchoolRepository.
+     */
     @InjectMocks
     private InventoryService inventoryService;
 
@@ -43,12 +53,42 @@ class InventoryServiceTest {
         inventory.setIsActive(true);
         inventory.setLocation("Sur");
 
+
+        /*
+        when: This is a Mockito method used to specify a behavior for a
+                mock object when a particular method is called.
+
+        schoolRepository.save(any(School.class)): This specifies that when the save method of
+                                                  schoolRepository is called with any School object,
+                                                  the behavior should be defined.
+
+        thenReturn(school): This specifies that the save method should return the school object
+                            when it is called with any School object.
+        */
+
+        /*
+        This line effectively tells Mockito that when the save method is called on
+        schoolRepository with any School object, it should return the predefined school object.
+        */
         when(inventoryRepository.save(any(Inventory.class))).thenReturn(inventory);
 
         InventoryDTO savedInventory = inventoryService.addInventory(inventory);
 
+
+        /*
+        verify: This is a Mockito method used to check if a particular method on a mock object was called.
+        schoolRepository: This is the mock object on which the method call is verified.
+        times(1): This specifies that the save method should be called exactly once.
+        save(any(School.class)): This specifies that the save method should be called with any School object.
+        */
+
+        /*
+        This line checks that the save method of
+        schoolRepository was called exactly once with any School object.
+        */
         verify(inventoryRepository, times(1)).save(any(Inventory.class));
 
+        // Assert the returned school matches the mock data
         assertEquals("Sulaiman", savedInventory.getInventoryManager());
     }
 
@@ -58,7 +98,7 @@ class InventoryServiceTest {
 
     @Test
     void deleteInventory() {
-    }
+        }
 
     @Test
     void getAll() {
