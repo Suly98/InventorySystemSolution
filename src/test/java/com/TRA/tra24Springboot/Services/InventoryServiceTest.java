7644bc78bb3f958e9290b400585557dba3336d1e
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-11 09:50:11 +0400

    added InventoryServiceTest

---
+++ src/test/java/com/TRA/tra24Springboot/Services/InventoryServiceTest.java
@@ -0,0 +1,24 @@
+package com.TRA.tra24Springboot.Services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InventoryServiceTest {
+
+    @Test
+    void addInventory() {
+    }
+
+    @Test
+    void getInventoryByManager() {
+    }
+
+    @Test
+    void deleteInventory() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
