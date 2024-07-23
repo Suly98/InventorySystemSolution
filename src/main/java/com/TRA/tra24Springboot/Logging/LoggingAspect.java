0efc76950de7d074393e9c8787edc468e12f9d02
Suly98 <135811486+Suly98@users.noreply.github.com>
2024-07-23 09:57:33 +0400

    did Time execution for logger

--- src/main/java/com/TRA/tra24Springboot/Logging/LoggingAspect.java
+++ src/main/java/com/TRA/tra24Springboot/Logging/LoggingAspect.java
@@ -8,20 +8,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.xml.crypto.Data;
+import java.util.Date;
+
 @Aspect
 @Component
 public class LoggingAspect {
     public static Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
-    @Pointcut(value = "execution(* com.TRA.tra24Springboot.Controllers.*(..)")
+    public static Long time;
+
+
+    @Pointcut(value = "execution(* com.TRA.tra24Springboot.Controllers.InventoryController.*(..))")
     public void pointCutDefinitionInventory(){}
 
     @Before(value = "pointCutDefinitionInventory()")
     public void logBefore(JoinPoint pjp){
+        time = System.nanoTime();
         System.out.println("Before method: " + pjp.getSignature().getName());
     }
 
-    @AfterReturning(value = "pointCutDefinitionInventory()")
+    @AfterReturning(value = "pointCutDefinitionInventory()", returning = "result")
     public void logAfterReturning(JoinPoint pjp, Object result){
         System.out.println("After method: " + pjp.getSignature().getName() + ", Result: " + result);
 
@@ -39,7 +46,11 @@ public class LoggingAspect {
                 + mapper.writeValueAsString(array));
 
         Object object = pjp.proceed();
-        logger.info("\n**** After function execution ****"+ "\nThis class is running: " + className + "\nThis function is running " + methodName + "()" + "\nResponse : "
+        Long duration = System.nanoTime() - time;
+        Float timeExecuted = duration/(1000000000.0f);
+
+        logger.info("\n**** After function execution ****"+ "\nTime Execution: " + timeExecuted + "sec"+
+                "\nThis class is running: " + className + "\nThis function is running " + methodName + "()" + "\nResponse : "
                 + mapper.writeValueAsString(object));
         return object;
     }
